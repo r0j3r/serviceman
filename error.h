fa156dc9d868cc82e1dc296c28f3a97c9328207f
rogelio <rogelio.serrano@gmail.com>
2014-08-21 14:04:10 +0100

    changed static const definitions to enum so i can use them in switch statements

--- error.h
+++ error.h
@@ -1,5 +1,7 @@
-
-static const int SUCCESS = 0;
-static const int E_INVALID_OP = 1;
-static const int E_UNKNOWN_PROTOCOL = 2;
-static const int E_SPAWN_FAILED = 3;
+enum err_code
+{
+    SUCCESS,
+    E_INVALID_OP,
+    E_UNKNOWN_PROTOCOL,
+    E_SPAWN_FAILED
+};
